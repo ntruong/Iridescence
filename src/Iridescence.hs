0d115b24858af31694b614cb37384b83803506db
ntruong <nt31415@gmail.com>
2019-01-27 16:47:02 -0600

    basic palette generation

--- src/Iridescence.hs
+++ src/Iridescence.hs
@@ -1,20 +1,47 @@
 module Iridescence where
 
-import Data.Either (either)
+import Data.List (sort)
+import Numeric (showHex)
 import System.IO (hFlush, stdout)
 import Codec.Picture
   ( PaletteCreationMethod(..)
   , PaletteOptions(..)
+  , PixelRGB8(..)
   , convertRGB8
   , palettize
   , readImage
   , writePng
   )
-import Codec.Picture.Types (ColorSpaceConvertible(..))
+import Codec.Picture.Types (generateImage, pixelFold)
 
 -- | Palletization options.
 opts :: PaletteOptions
-opts = PaletteOptions MedianMeanCut True 10
+opts = PaletteOptions MedianMeanCut True 8
+
+-- | Bound an (ordinal) value.
+clamp :: (Ord a) => a -> a -> a -> a
+clamp a b = min b . max a
+
+-- | Darken a pixel.
+darken :: Float -> PixelRGB8 -> PixelRGB8
+darken a (PixelRGB8 r g b) =
+  let ratio = clamp 0 1 a
+      f x = fromIntegral . floor $ a * fromIntegral x
+  in  PixelRGB8 (f r) (f g) (f b)
+
+-- | Lighten a pixel.
+lighten :: Float -> PixelRGB8 -> PixelRGB8
+lighten a (PixelRGB8 r g b) =
+  let ratio = clamp 0 1 a
+      f x = fromIntegral . floor $
+        fromIntegral x + (255 - fromIntegral x) * ratio
+  in  PixelRGB8 (f r) (f g) (f b)
+
+-- | Get a hex string from an RGB pixel.
+hex :: PixelRGB8 -> String
+hex (PixelRGB8 r g b) =
+  let f x = showHex (fromIntegral x) ""
+  in  concat [f r, f g, f b]
 
 app :: IO ()
 app = do
@@ -25,5 +52,12 @@ app = do
   case img of
     Left _ -> putStrLn " [~] failed to read image."
     Right rgb -> do
-      let palette = (snd . palettize opts . convertRGB8) rgb
+      let rawPalette = (snd . palettize opts . convertRGB8) rgb
+          colors :: [PixelRGB8]
+          colors = sort $ pixelFold (const . const . flip (:)) [] rawPalette
+          lightcolors = lighten 0.4 <$> colors
+          theme = colors ++ lightcolors
+          palette = generateImage (const . (!!) theme) 16 1
+      putStrLn " [~] colors: \n"
+      putStrLn $ concat (flip (++) "\n" . hex <$> theme)
       writePng "palette.png" palette
